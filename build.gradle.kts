11b25e29cb5ce441608cc9bdf667bce29370556d
sohyun <sohyun.ku@nts-corp.com>
2023-03-29 14:42:28 +0900

    Add Gradle release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import net.researchgate.release.ReleaseExtension
 
 plugins {
     id("org.springframework.boot") version "2.7.9"
     id("io.spring.dependency-management") version "1.1.0"
+    id("net.researchgate.release") version "3.0.2"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "com.example"
-version = "1.0.0"
-java.sourceCompatibility = JavaVersion.VERSION_17
+version = requireNotNull(property("version"))
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -21,10 +22,19 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+configure<ReleaseExtension> {
+    val releaseVersion = if (hasProperty("release.releaseVersion")) {
+        requireNotNull(property("release.releaseVersion"))
+    } else {
+        version
+    }
+
+    ignoredSnapshotDependencies.set(listOf("net.researchgate:gradle-release"))
+    pushReleaseVersionBranch.set("release/${ releaseVersion }")
+    tagTemplate.set("v${ releaseVersion }")
+    newVersionCommitMessage.set("Update next development version after Release")
+    with(git) {
+        requireBranch.set("master")
     }
 }
 
